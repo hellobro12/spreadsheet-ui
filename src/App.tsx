ee3a5f51bf812d8ed6f90471b14a1786515c5808
Shreyash <91046521+hellobro12@users.noreply.github.com>
2025-07-16 19:26:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderBar from './components/HeaderBar';
 import Toolbar from './components/Toolbar';
 import SpreadsheetTable from './components/SpreadsheetTable';
-
+#to load spreadsheet
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
